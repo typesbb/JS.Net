b7762ab746c140f1ea8d4521aff7f3804b917a0c
typesbb <typesbb@qq.com>
2016-05-10 14:37:33 +0800

    pack nuget

--- JS.Net/Properties/AssemblyInfo.cs
+++ JS.Net/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("JS.Net")]
-[assembly: AssemblyDescription("一个用C#来描述JS的库，用C#来写JS。")]
+[assembly: AssemblyDescription("A .net lib that you can writing javascript in c#.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("typesbb")]
 [assembly: AssemblyProduct("JS.Net")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.1")]
-[assembly: AssemblyFileVersion("0.0.1.1")]
+[assembly: AssemblyVersion("0.0.1.2")]
+[assembly: AssemblyFileVersion("0.0.1.2")]
